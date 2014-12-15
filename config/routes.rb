f4e35ebd43a0f8c0bb2534f87c7b8bf673923fb9
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-12-15 09:34:48 -0800

    Added admin dashboard for coord objects

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => "catalog#index"
   blacklight_for :catalog
+
+  get '/admin', :to => 'admin#index', :as => "admin_index"
+
+  namespace :admin do
+    resources :coordinates
+  end
 end
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,12 +1,37 @@
 require 'spec_helper'
 
 describe "index page" do
+
+  let(:user) {create(:user, :admin)}
+
   context "when on the index page" do
     before do
       visit root_path
     end
     it "should say building oregon" do
       expect(page).to have_content("Building Oregon")
+    end
+
+    context "logged in as admin and the admin panel link is clicked" do
+      before do
+        capybara_login(user) if user
+        visit root_path
+        click_link 'Admin Panel'
+      end
+
+      it 'should take you to the admin panel page' do
+        expect(page).to have_content('Administration Dashboard')
+      end
+    end
+
+    context 'not logged in as admin and the admin panel is accessed' do
+      before do
+        visit admin_index_path
+      end
+
+      it 'should not allow access' do
+        expect(page).to have_content('Only admin can access')
+      end
     end
   end
 end
