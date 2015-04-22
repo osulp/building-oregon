6945e73a255dfa9e9c6a30a2471358664e9d275f
Brandon Straley <straleyb@10-249-106-134.wireless.oregonstate.edu>
2015-04-22 13:23:55 -0700

    Removed useless coordinate files and table

--- app/models/coordinate.rb
+++
@@ -1,5 +0,0 @@
-class Coordinate < ActiveRecord::Base
-  validates_presence_of :solr_id
-  validates_presence_of :lat
-  validates_presence_of :long
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,60 +11,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229220242) do
+ActiveRecord::Schema.define(version: 20150422201302) do
 
-  create_table "bookmarks", force: true do |t|
-    t.integer  "user_id",       null: false
-    t.string   "user_type"
-    t.string   "document_id"
-    t.string   "title"
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer  "user_id",                   null: false
+    t.string   "user_type",     limit: 255
+    t.string   "document_id",   limit: 255
+    t.string   "title",         limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "document_type"
+    t.string   "document_type", limit: 255
   end
 
   add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
 
-  create_table "coordinates", force: true do |t|
-    t.decimal  "lat"
-    t.decimal  "long"
-    t.integer  "solr_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "searches", force: true do |t|
+  create_table "searches", force: :cascade do |t|
     t.text     "query_params"
     t.integer  "user_id"
-    t.string   "user_type"
+    t.string   "user_type",    limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "searches", ["user_id"], name: "index_searches_on_user_id"
 
-  create_table "settings", force: true do |t|
-    t.string   "setting_name"
+  create_table "settings", force: :cascade do |t|
+    t.string   "setting_name", limit: 255
     t.text     "value"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",                      default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "guest",                  default: false
-    t.boolean  "admin",                  default: false
+    t.boolean  "guest",                              default: false
+    t.boolean  "admin",                              default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
