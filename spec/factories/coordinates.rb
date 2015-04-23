51971f140e72339bbc905ace47ea8c5f174825b4
Brandon Straley <straleyb@straleyb-mbp15.tss.oregonstate.edu>
2015-04-23 08:15:20 -0700

    Deleted forgotten model spec

--- spec/factories/coordinates.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :coordinate do
-    lat "9.99"
-    long "9.99"
-    solr_id 1
-  end
-end
--- spec/models/coordinate_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Coordinate do
-  it{should validate_presence_of(:solr_id)}
-  it{should validate_presence_of(:lat)}
-  it{should validate_presence_of(:long)}
-end
