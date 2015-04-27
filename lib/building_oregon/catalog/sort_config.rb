9c59adb4cd0da1388934417be5abe8f5b0ccd6fa
Brandon Straley <straleyb@straleyb-mbp15.tss.oregonstate.edu>
2015-04-27 14:05:30 -0700

    Added more sort fields

--- lib/building_oregon/catalog/sort_config.rb
+++ lib/building_oregon/catalog/sort_config.rb
@@ -5,6 +5,9 @@ module BuildingOregon
       included do
         configure_blacklight do |config|
           config.add_sort_field "score desc", :label => "Relevance"
+          config.add_sort_field "sort_date_desc_dtsi asc", :label => "Date Decending"
+          config.add_sort_field "sord_date_asc_dtsi asc", :label => "Date Ascending"
+          config.add_sort_field "system_create_dtsi desc", :label => "Recently Added"
         end
       end
     end
