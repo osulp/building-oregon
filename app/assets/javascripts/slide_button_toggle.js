5c222579e0f115733160e0c333e9e8f7230d136e
Brandon Straley <straleyb@straleyb-mbp15.tss.oregonstate.edu>
2015-04-28 08:49:57 -0700

    Added checking of Id instead of catalog in path

--- app/assets/javascripts/slide_button_toggle.js
+++ app/assets/javascripts/slide_button_toggle.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var path = window.location.pathname;
-  if(path.indexOf("catalog") > -1){
+  if(path.indexOf("oregondigital:") > -1){
     document.getElementById("button").style.visibility = "hidden";
   }
 });
