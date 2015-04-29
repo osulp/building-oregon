7986cdd06f080efb4df9c677f5df7275e3e20160
Trey Terrell <trey.terrell@oregonstate.edu>
2015-04-29 15:46:46 -0700

    Clean up footer a little bit, improve resizing JS.

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -1,10 +1,20 @@
 jQuery(resizeMain);
 $(window).resize(resizeMain);
 function resizeMain(){
+  $("#blacklight-map").css("height", 0)
+  $("#main-container").css("min-height",0)
   var windowHeight = $(window).height();
+  var navbarHeight = $('#header-navbar').outerHeight(true);
   var footerHeight = $('#dynamicFooter').outerHeight(true);
-  var containerPos = $('#main-container').offset().top;
-  var total = windowHeight-containerPos-footerHeight;
-  $('#main-container').css('min-height', total);
-  $('#blacklight-map').css('height', total);
+  var links_footer = $("#dynamicFooter .links").outerHeight(true)
+  var logoHeight = 45
+  if(footerHeight < links_footer+logoHeight) {
+    footerHeight = links_footer+logoHeight;
+  }
+  var containerHeight = $('#main-container').outerHeight(true);
+  var total = windowHeight-footerHeight-navbarHeight;
+  if(total > 0) {
+    $('#main-container').css('min-height', total);
+  }
+  $('#blacklight-map').css('height', total-containerHeight);
 }
