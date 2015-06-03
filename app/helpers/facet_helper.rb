d245a1fe772f178d3f3682b4592cb726ad3867ee
Brandon Straley <straleyb@STRALEYB-MBP15.tss.oregonstate.edu>
2015-06-03 11:00:57 -0700

    changes the index information and formats, makes index picture a link, removes comma

--- app/helpers/facet_helper.rb
+++ app/helpers/facet_helper.rb
@@ -3,4 +3,13 @@ module FacetHelper
     BuildingOregon::ControlledValue.new(label).to_s
   end
 
+  def controlled_index_label(label)
+    @value_array = []
+    label[:value].each do |value|
+      @value_array.append(BuildingOregon::ControlledValue.new(value).to_s)
+    end
+    @value_array
+  end
+ 
+
 end
