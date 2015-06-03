e9826495c4dd5a4a26c934dc88db2226374ab8a1
Brandon Straley <straleyb@STRALEYB-MBP15.tss.oregonstate.edu>
2015-06-03 13:14:08 -0700

    Changed font sizes and swapped glyphicon

--- app/assets/javascripts/toggleGlyphicon.js
+++
@@ -1,7 +0,0 @@
-function toggleGlyphicon(){
-  if($('.glyphicon-chevron-right').length > 0){
-    $('.glyphicon-chevron-right').toggleClass("glyphicon-chevron-right glyphicon-chevron-left");
-  } else if($('.glyphicon-chevron-left').length > 0){
-    $('.glyphicon-chevron-left').toggleClass("glyphicon-chevron-right glyphicon-chevron-left"); 
-  }
-}
