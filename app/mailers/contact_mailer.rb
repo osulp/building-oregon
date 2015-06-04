9ca4d4cf58352bad6ee14eb4431658a8b65bec83
Brandon Straley <straleyb@STRALEYB-MBP15.tss.oregonstate.edu>
2015-06-04 16:31:10 -0700

    Added more tests and extended to have configurable email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,11 @@ class ContactMailer < ActionMailer::Base
   def contact_email(email, message)
     @email = email
     @message = message
-    mail(:to => "brandon.straley@oregonstate.edu", :subject => "General Inquiry", :from => @email)
+    binding.pry
+    if Setting.contact.blank?
+      mail(:to => "margaret.mellinger@oregonstate.edu" , :subject => "General Inquiry", :from => @email)
+    else
+      mail(:to => Setting.contact, :subject => "General Inquiry", :from => @email)
+    end
   end
 end
--- spec/mailers/contact_spec.rb
+++ spec/mailers/contact_spec.rb
@@ -47,5 +47,33 @@ describe "Contact Us" do
         expect(page).to have_field("Message", :with => "test message")
       end
     end
+    context "When there is no setting configured for email" do
+     let(:setting){create(:setting, :setting_name => 'Contact', :value => '')} 
+     before do
+        setting
+        visit contact_us_path
+        fill_in "Email", :with => "test@email.com"
+        fill_in "Message", :with => "test message"
+        click_button "Submit"
+      end
+      it "should use the base email" do
+        expect(page).to have_content("Thank you for contacting us!")
+        expect(ActionMailer::Base.deliveries.first.to.first).to eq "margaret.mellinger@oregonstate.edu"
+      end
+    end
+    context "When there is a setting configured for email" do
+      let(:setting){create(:setting, :setting_name => 'Contact', :value => 'blah@blah.com')} 
+      before do
+        setting
+        visit contact_us_path
+        fill_in "Email", :with => "test@email.com"
+        fill_in "Message", :with => "test message"
+        click_button "Submit"
+      end
+      it "should use the configured email" do
+        expect(page).to have_content("Thank you for contacting us!")
+        expect(ActionMailer::Base.deliveries.first.to.first).to eq "blah@blah.com"
+      end
+    end
   end
 end
