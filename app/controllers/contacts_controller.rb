3903148a8125219805bb40543498d021c4a09470
Trey Terrell <trey.terrell@oregonstate.edu>
2015-06-05 16:00:50 -0700

    Form object.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,35 +1,25 @@
 class ContactsController < ApplicationController
 
   def index
+    @contact = ContactEmail.new
   end
   
   def create
-    @email ||= contact_params["email"]
-    @message = contact_params["message"]
-    if @email.blank?
+    @contact = ContactEmail.new(contact_params)
+    unless @contact.valid?
       flash[:error] = "Please enter an email address and try again"
       render :index
     else
-      flash[:success] = "Thank you for contacting us!" if mailer.deliver_now
+      if @contact.deliver
+        flash[:success] = "Thank you for contacting us!"
+      end
       redirect_to root_path
     end
   end
 
   private
 
-  def mailer
-    if Setting.contact.blank?
-      contact_mailer.contact_email(@email, @message, I18n.t('mailer.default_email')) 
-    else
-      contact_mailer.contact_email(@email, @message, Setting.contact) 
-    end
-  end
-
-  def contact_mailer
-    ContactMailer
-  end
-
   def contact_params
-    params["Contact"]
+    params.require(:contact_email).permit(:email, :message).to_h.symbolize_keys
   end 
 end
