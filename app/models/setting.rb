665b0f37b7b4400e0a97079ee239798e6161b1c8
Brandon Straley <straleyb@STRALEYB-MBP15.tss.oregonstate.edu>
2015-06-05 16:36:02 -0700

    Made settings actually set default values

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -12,11 +12,19 @@ class Setting < ActiveRecord::Base
 
   def self.create_defaults
     default_settings.each do |setting_name, settings|
-      self.where(:setting_name => setting_name).first_or_create
+      self.where(:setting_name => setting_name).first_or_create(:value => value(settings))
     end
   end
 
+  def self.value(settings)
+    settings["tag_attributes"]["default"]
+  end
+
+  def self.method_missing_value(new_string)
+    @default_settings[new_string]["tag_attributes"]["default"]
+  end
+
   def self.setting_value(new_string)
-    self.where(:setting_name => new_string).first_or_initialize.value.to_s
+    self.where(:setting_name => new_string).first_or_initialize(:value => method_missing_value(new_string)).value.to_s
   end
 end
