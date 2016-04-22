773e4ed5970d6da6cd3c6084b1aa3b91c8dde681
Brandon Straley <straleyb@onid.oregonstate.edu>
2016-04-22 14:52:37 -0700

    Remove hardcoded keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,7 @@
 Recaptcha.configure do |config|
   config_file = YAML.load(ERB.new(File.read("#{Rails.root}/config/recaptcha.yml")).result)
-  config.public_key  = config_file['recaptcha']['public_key'] || '6LetGR4TAAAAAPsDKUwnVJPLlJfNKWYmjruV8gPE'
-  config.private_key = config_file['recaptcha']['private_key'] || '6LetGR4TAAAAADKju7q1OJaCc465e96ne9PfF8Pg'
+  config.public_key  = config_file['recaptcha']['public_key']
+  config.private_key = config_file['recaptcha']['private_key']
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
