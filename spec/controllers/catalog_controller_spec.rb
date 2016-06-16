636e751bad61e0bd88e4c5695e7f43a7777b0e9b
Brandon Straley <straleyb@onid.oregonstate.edu>
2016-06-16 14:05:20 -0700

    Unrestrict image search to work with uri only instead of format

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe CatalogController do
       end
     end
     let(:document) do
-      {:id => "good", :reviewed_ssim => "true", :desc_metadata__set_sim => "http://oregondigital.org/resource/oregondigital:building-or", :desc_metadata__latitude_teim => "9001", :desc_metadata__format_label_sim => "image/tiff$http://purl.org/NET/mediatypes/image/tiff"}
+      {:id => "good", :reviewed_ssim => "true", :desc_metadata__set_sim => "http://oregondigital.org/resource/oregondigital:building-or", :desc_metadata__latitude_teim => "9001", :desc_metadata__format_sim => "http://purl.org/NET/mediatypes/image/tiff"}
     end
     def bad_document(attributes={})
       document.merge({:id => "bad"}.merge(attributes))
@@ -63,7 +63,7 @@ RSpec.describe CatalogController do
     context "when there are documents that are non image ones" do
       before do
         solr.add(document)
-        solr.add(bad_document(:desc_metadata__format_label_sim => "PDF$http://Blah.com"))
+        solr.add(bad_document(:desc_metadata__format_sim => "http://Blah.com"))
         solr.commit
         get :index
       end
