487a5d6c550259789cb5c803d2c909d5094f773e
Brandon Straley <straleyb@onid.oregonstate.edu>
2017-09-14 11:25:09 -0700

    changes production to not use god

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,43 +10,7 @@ role :db,  config['hosts']['db'], :primary => true # This is where Rails migrati
 # Git Config
 set :branch, config['branch']
 # God Settings
-set(:god_app_path) {"#{current_path}/#{config['god']['app_path']}"}
-set :god_sites_path,  config['god']['sites_path']
 set :deploy_to, config['deploy_to']
 set :rails_env, :production
 # Deploy Commands
 # Override deploy to inform god to do the restarts.
-namespace :deploy do
-  task :start do
-    god.start
-  end
-  task :stop do
-    god.stop
-  end
-  task :restart do
-    god.restart
-  end
-end
-
-namespace :god do
-  desc "Reload god config"
-  task :reload, :roles => :app, :except => {:no_release => true} do
-    run "ln -nfs #{god_app_path} #{god_sites_path}/#{application}.conf"
-    sudo "/etc/init.d/god restart"
-  end
-
-  task :restart, :roles => :app, :except => {:no_release => true} do
-    run "touch #{shared_path}/tmp/restart.txt"
-  end
-
-  task :start, :roles => :app do
-    sudo "/etc/init.d/god startapp #{application}"
-  end
-
-  task :stop, :roles => :app do
-    sudo "/etc/init.d/god stopapp #{application}"
-  end
-  task :status, :roles => :app do
-    sudo "/etc/init.d/god status #{applictaion}"
-  end
-end
